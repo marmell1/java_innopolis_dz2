86150da3ecb03a79dc8824ee1cd847cef49e44e3
Marina Melekhova <marmell2017@gmail.com>
2025-07-27 12:57:16 +0300

    add dz3_1.java

---
+++ dz3_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class dz3_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in, "cp866");
+        System.out.println("Введите Ваше имя");
+        String name1 = scanner1.nextLine();
+
+        System.out.println("Привет, "+ name1);
+    }
+
+}
