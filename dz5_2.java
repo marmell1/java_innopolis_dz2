cf494e1464a248e11a1d9d6ce9dfd6d5e31675ee
Marina Melekhova <marmell2017@gmail.com>
2025-08-01 19:10:40 +0300

    add dz5_2.java

---
+++ dz5_2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class dz5_2 {
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in, "cp866");
+        System.out.println();
+        System.out.println("Введите последовательность символов, не более 106");
+        String sSymbols = scanner1.nextLine();
+        if (sSymbols.length()>106){System.out.println("Введено слишком большое количество символов");}
+        else{
+            int nCount = 0;
+            System.out.println("Могут ли стрелы пересекаться? Если да, то >>-->>--> - это две стрелы, если нет, то одна");
+            System.out.println("Введите 1 если не могут пересекаться, 0 - если могут");
+            Scanner scanner2 = new Scanner(System.in, "cp866");
+            String sCross = scanner2.nextLine();
+            int nCross = Integer.valueOf(sCross);
+            int nDelta = 1;
+
+            for (int i =0;i<(sSymbols.length()-4);i= i+nDelta){
+                nDelta=1;
+                //System.out.println("i " +i);
+                int nIndexR = sSymbols.indexOf(">>-->",i);
+                //System.out.println("nIndexR " + nIndexR);
+                int nIndexL = sSymbols.indexOf("<--<<",i);
+                //System.out.println("nIndexL " + nIndexL);
+                if (nIndexR==i){nCount++;nDelta=nDelta+(5*nCross);}
+                //System.out.println(nCount);
+                if (nIndexL==i){nCount++;;nDelta=nDelta+(5*nCross);};
+                //System.out.println(nCount);
+            }
+            System.out.println("Итоговое количество стрел - " +nCount);
+            }
+
+       
+        scanner1.close();
+    }
+}
+
+/*
+В рамках данной задачи, считаем, что стрела могут накладываться друг на друга, 
+то есть последовательность ">>-->>-->" - это две стрелы.
+
+Задача 2. Задана последовательность, состоящая только из символов ‘>’, ‘<’ и ‘-‘. Требуется найти количество стрел, 
+которые спрятаны в этой последовательности. Стрелы – это подстроки вида ‘>>-->’ и ‘<--<<’.
+Входные данные: в первой строке входного потока записана строка, состоящая из символов ‘>’, ‘<’ и ‘-‘ (без пробелов). 
+Строка может содержать до 106 символов.
+Выходные данные: в единственную строку выходного потока нужно вывести искомое количество стрелок.
+
+ */
